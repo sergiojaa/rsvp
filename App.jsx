a8010dddd50407545defebc4a6669eb33decfdd4
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-13 00:53:12 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-dddddd
+dddddddddd
 function App() {
   const [input ,setInput] = useState('')
   const [submitted, setSubmitted] = useState(false)
